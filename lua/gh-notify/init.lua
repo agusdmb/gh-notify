edaca8024b92c90362edec3f347126b2c1e38d4e
Agustin Marquez <agusdmb@gmail.com>
2023-05-07 19:58:49 +0200

    get user from gh cli

--- lua/gh-notify/init.lua
+++ lua/gh-notify/init.lua
@@ -6,7 +6,7 @@ local function check_for_new_prs(user)
   local result = handle:read("*a")
   handle:close()
 
-  local data = vim.fn.json_decode(result)
+  local data = vim.json.decode(result)
 
   if #data > 0 then
     local message = string.format("New PRs assigned to %s:", user)
@@ -17,12 +17,21 @@ local function check_for_new_prs(user)
   end
 end
 
+local function get_gh_username()
+  local handle = io.popen("gh api user")
+  local result = handle:read("*a")
+  handle:close()
+
+  return vim.json.decode(result)
+end
+
 function M.setup(opts)
-  local user = opts.user
+  local user = get_gh_username()
   local interval = opts.interval or 60
+  local start_after = opts.start_after or 10
 
   local timer = vim.loop.new_timer()
-  timer:start(10 * 1000, interval * 1000, vim.schedule_wrap(function() check_for_new_prs(user) end))
+  timer:start(start_after * 1000, interval * 1000, vim.schedule_wrap(function() check_for_new_prs(user.login) end))
 end
 
 return M
